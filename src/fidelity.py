accc9e5d101eb266f38456cbf3f98812ca448c6c
David Glandon <david.glandon@gmail.com>
2012-06-04 07:22:05 -0400

    continuted improvements.  Identifying ntf funds, fidelity fund picks, and unavailable funds

--- src/fidelity.py
+++ src/fidelity.py
@@ -9,6 +9,7 @@ url = urllib.urlopen(baseUrl + 'fund-families-no-transaction-fee')
 page = url.read()
 
 symbols  = []
+fundpicks = []
 
 familyLinks = re.findall(r'"funds-by-family\?family=\w+"',page)
 familyLinks = [x[1:len(x)-1] for x in familyLinks]
@@ -25,10 +26,38 @@ for family in familyLinks:
         url = urllib.urlopen(baseUrl + fund)
         page = url.read()
 
-        symbol = re.search(r'SECURITY_ID=\w+&', page).group()
+        symbol = re.search(r'SECURITY_ID=\w+&', page)
+        if symbol is None:
+            continue
+        symbol = symbol.group()
         symbol = symbol[12:len(symbol)-1]
         sys.stderr.write('Fund: %s\n' % symbol)
-        
-        symbols.append(symbol) 
+   
+        ntf = re.search(r'ntf.gif',page)
+        if ntf is None:
+            sys.stderr.write('Not NTF\n')
+            continue
 
+        closed = re.search(r'closed to new', page)
+        if closed is not None:
+            sys.stderr.write('Closed to new investors\n')
+            continue
+
+     
+        fpa = re.search(r'Fidelity Portfolio Advisor',page)
+        if fpa is not None:
+            sys.stderr.write('Throwing out Fidelity Portfolio Advisor\n')
+            continue
+  
+        ffp = re.search(r'fund_pick.gif',page)
+        if ffp is not None:
+            fundpicks.append(symbol)
+        
+        print '%s,' % symbol
+        symbols.append(symbol)
+
+print '\nfundpicks = '
+print fundpicks
+
+print '\ntf = '
 print symbols
