9a3332e718f31f87f8f6a3c7cb1431df20a53f4e
David Glandon <david.glandon@gmail.com>
2012-07-01 19:18:02 -0400

    Modified fidelity.py to add items to the "funds.sqlite" database.  The Availability table is not updated using this script.  I added these manually (python) because I don't expect the fideliy.py script to be run any more.  I also manually added the paychex entries.
    fund.py contains the sqlalchemy classes.  We may want to wrap these better than what they are now.
    
    funds.sqlite is the current state of the database.  I have not yet put the historical prices into the table.

--- src/fidelity.py
+++ src/fidelity.py
@@ -1,6 +1,16 @@
 import urllib
 import re
 import sys
+import sqlalchemy
+import sqlalchemy.ext.declarative
+import sqlalchemy.orm
+import fund
+
+engine = sqlalchemy.create_engine('sqlite:///funds.sqlite', echo=True)
+Session = sqlalchemy.orm.sessionmaker(bind=engine)
+session = Session()
+
+dbFunds = []
 
 baseUrl = 'http://fundresearch.fidelity.com/mutual-funds/'
 
@@ -24,8 +34,9 @@ for family in familyLinks:
     fundLinks = re.findall(r'"summary/\w+"', page)
     fundLinks = [x[1:len(x)-1] for x in fundLinks]
     
-    for fund in fundLinks:
-        url = urllib.urlopen(baseUrl + fund)
+    for fundlink in fundLinks:
+
+        url = urllib.urlopen(baseUrl + fundlink)
         page = url.read()
         url.close()
 
@@ -35,6 +46,10 @@ for family in familyLinks:
         symbol = symbol.group()
         symbol = symbol[12:len(symbol)-1]
         sys.stderr.write('Fund: %s\n' % symbol)
+
+        #need to add name
+        dbFund = fund.Fund(symbol, '')
+        dbFund.FundDataSources.append(fund.FundDataSource(baseUrl + fundlink))
    
         ntf = re.search(r'ntf.gif',page)
         if ntf is None:
@@ -49,8 +64,8 @@ for family in familyLinks:
      
         fpa = re.search(r'Fidelity Portfolio Advisor',page)
         if fpa is not None:
-            sys.stderr.write('Throwing out Fidelity Portfolio Advisor\n')
-            continue
+            sys.stderr.wite('Throwing out Fidelity Portfolio Advisor\n')
+        
   
         ffp = re.search(r'fund_pick.gif',page)
         if ffp is not None:
@@ -58,6 +73,11 @@ for family in familyLinks:
         
         print '%s,' % symbol
         symbols.append(symbol)
+
+        session.add(dbFund)
+        fund.FundTable(session).add(dbFund)
+session.commit()
+session.close()
 
 print '\nfundpicks = '
 print fundpicks
---
+++ src/fund.py
@@ -0,0 +1,63 @@
+import urllib
+import re
+import sys
+import sqlalchemy
+import sqlalchemy.ext.declarative
+import sqlalchemy.orm
+
+__Base = sqlalchemy.ext.declarative.declarative_base()
+class Fund(__Base):
+    __tablename__ = 'Funds'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    symbol = sqlalchemy.Column(sqlalchemy.String, unique=True)
+    name = sqlalchemy.Column(sqlalchemy.String)
+    def __init__(self, symbol, name):
+        self.symbol = symbol
+        self.name = name
+
+    def __repr__(self):
+        return "<Fund('%s = %s')>" % (self.symbol, self.name)
+
+class FundTable(object):
+    def __init__(self, session):
+        self.session = session
+    def add(self, fund):
+        if not isinstance(fund, Fund):
+            raise TypeError
+        if self.session.query(Fund).filter_by(symbol=fund.symbol).count() == 0:
+            self.session.add(fund)
+    def getFunds(self):
+        return self.session.query(Fund)
+
+
+class FundDataSource(__Base):
+    __tablename__ = 'FundDataSources'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    fund_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('Funds.id'))
+    fund = sqlalchemy.orm.relationship('Fund', backref=sqlalchemy.orm.backref('FundDataSources', order_by=id))
+    url = sqlalchemy.Column(sqlalchemy.String)
+    def __init__(self, url):
+        self.url = url
+
+class HistoricalPrice(__Base):
+    __tablename__ = 'HistoricalPrices'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    date = sqlalchemy.Column(sqlalchemy.Date, unique=True)
+    price = sqlalchemy.Column(sqlalchemy.Float)
+    fund_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('Funds.id'))
+    fund = sqlalchemy.orm.relationship('Fund', backref=sqlalchemy.orm.backref('HistoricalPrices', order_by=id))
+
+class Availability(__Base):
+    __tablename__ = 'Availability'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key = True)
+    fund_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('Funds.id'))
+    fund = sqlalchemy.orm.relationship('Fund', backref=sqlalchemy.orm.backref('Availability', order_by=id))
+    name = sqlalchemy.Column(sqlalchemy.String)
+
+    def __init__(self, name):
+        self.name = name
+
+def createFundTable(filename):
+    engine = sqlalchemy.create_engine('sqlite:///' + filename, echo=True)
+    __Base.metadata.create_all(engine)
+    return engine
